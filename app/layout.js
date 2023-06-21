f215329de0f1c5cff1eaf2483e34210ff4028888
Gonza <gonzagramaglia@gmail.com>
2023-06-21 15:49:46 -0300

    TwitHub Theme

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,18 @@
 import 'tailwindcss/tailwind.css';
 
 export const metadata = {
-  title: 'Twithub',
+  title: 'TwitHub',
   description: 'Social network for developers',
+  icons: {
+    icon: '/twithub-logo.ico'
+  }
 }
  
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
-      <head>
-        <link rel="icon" type="image/png" href="../public/twithub-logo.png"></link>
-      </head>
-      <body className='grid place-content-center min-h-screen bg-blue-100' >
-        <div className='bg-white text-6xl w-[100vw] sm:w-[520px] h-[100vh] sm:h-[90vh] sm:rounded-[5px]' >
+      <body className='grid place-content-center min-h-screen bg-[#03040b]' >
+        <div className='bg-[#0f1017] text-white text-6xl w-[100vw] sm:w-[520px] h-[100vh] sm:h-[90vh] sm:rounded-[5px]' >
           {children}
         </div>
       </body>
